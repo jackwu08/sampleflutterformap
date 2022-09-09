f2cdae16c09e900cee76de25442549624cc7487c
jack wu <104415289+jackwu08@users.noreply.github.com>
2022-09-09 16:17:04 +0800

    update order page2

--- lib/src/orderpage.dart
+++ lib/src/orderpage.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:window_size/window_size.dart';
+import './catalog.dart';
+import './item_tile.dart';
+
 
 class OrderPage extends StatelessWidget {
   const OrderPage({super.key});
@@ -8,8 +13,34 @@ class OrderPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       
-      body: Text(
-      'Index 3:Order Page',
+      body:  Selector<Catalog, int?>(
+        // Selector is a widget from package:provider. It allows us to listen
+        // to only one aspect of a provided value. In this case, we are only
+        // listening to the catalog's `itemCount`, because that's all we need
+        // at this level.
+        selector: (context, catalog) => catalog.itemCount,
+        builder: (context, itemCount, child) => ListView.builder(
+          // When `itemCount` is null, `ListView` assumes an infinite list.
+          // Once we provide a value, it will stop the scrolling beyond
+          // the last element.
+          itemCount: itemCount,
+          padding: const EdgeInsets.symmetric(vertical: 18),
+          itemBuilder: (context, index) {
+            // Every item of the `ListView` is individually listening
+            // to the catalog.
+            var catalog = Provider.of<Catalog>(context);
+
+            // Catalog provides a single synchronous method for getting
+            // the current data.
+            var item = catalog.getByIndex(index);
+
+            if (item.isLoading) {
+              return const LoadingItemTile();
+            }
+
+            return ItemTile(item: item);
+          },
+        ),
       ),
     );
   }
