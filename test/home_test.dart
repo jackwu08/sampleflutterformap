43aa2e4fc9251d34ebe222c2d24adc40c32f8bee
jack wu <104415289+jackwu08@users.noreply.github.com>
2022-09-13 17:36:29 +0800

    test2

--- test/home_test.dart
+++ test/home_test.dart
@@ -16,6 +16,38 @@ void main() {
       // Verify if the appropriate message is shown.
       expect(find.text('Infinite List'), findsOneWidget);
   });
+   testWidgets('Infinite list smoke test', (tester) async {
+    await _startapp(tester, 'root', 'password');
+    // Tap the first item's icon to add it to favorites.
+      await tester.tap(find.byIcon(Icons.home).first);
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      
+    const loadingDuration = Duration(milliseconds: 500);
+
+    // At first, the catalog shows only "..." (loading items).
+    expect(find.text('...'), findsWidgets);
+    expect(find.text('Color #1'), findsNothing);
+
+    await tester.pump(loadingDuration);
+
+    // After the app has had a chance to load items, it should no longer
+    // show "...".
+    expect(find.text('...'), findsNothing);
+    expect(find.text('Color #1'), findsOneWidget);
+
+    // Flinging up quickly (i.e. scrolling down).
+    await tester.fling(find.byType(ListView), const Offset(0, -2000), 5000);
+
+    // As we scroll down, we should see more items loading.
+    expect(find.text('...'), findsWidgets);
+    // The item at the top should no longer be in view.
+    expect(find.text('Color #1'), findsNothing);
+
+    // This last part is just giving the app time to complete its "fetch"
+    // of new items. Otherwise, the test fails because of outstanding
+    // asynchronous callbacks.
+    await tester.pump(loadingDuration);
+  });
 
 }
 
